b905b28ee5b0aed749f58cc666dffef551077b33
Nikolay <priddev@gmail.com>
2022-11-12 02:36:37 +0700

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+"use strict";
+
+const gulp = require("gulp");
+const webpack = require("webpack-stream");
+const browsersync = require("browser-sync");
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const htmlmin = require('gulp-htmlmin');
+
+const dist = "./dist/";
+//const dist = "/Programming/MAMP/htdocs/test";
+
+gulp.task("copy-html", () => {
+    return gulp.src("./src/*.html")
+                .pipe(htmlmin({ collapseWhitespace: true }))
+                .pipe(gulp.dest(dist))
+                .pipe(browsersync.stream());
+});
+
+gulp.task("styles", () => {
+  return gulp.src("src/assets/sass/**/*.+(scss|sass)")
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(rename({suffix: '.min', prefix: ''}))
+      .pipe(autoprefixer())
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest("dist/assets/css"))
+      .pipe(browsersync.stream());
+});
+
+gulp.task("build-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'development',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    watch: false,
+                    devtool: "source-map",
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: true,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+
+                }))
+                .pipe(gulp.dest(dist))
+                .on("end", browsersync.reload);
+});
+
+gulp.task("copy-assets", () => {
+    return gulp.src(['./src/assets/**/*.*', '!./src/assets/sass/**/*.*'])
+                .pipe(gulp.dest(dist + "/assets"))
+                .on("end", browsersync.reload);
+});
+
+gulp.task("watch", () => {
+    browsersync.init({
+      server: {
+        baseDir: "./dist/",
+        serveStaticOptions: {
+          extensions: ["html"]
+        }
+      }, 
+      port: 4000,
+      notify: true,
+      browser: 'firefox'
+    });
+    
+    gulp.watch("./src/*.html", gulp.parallel("copy-html"));
+    gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
+    gulp.watch("./src/assets/sass/**/*.+(scss|sass|css)",gulp.parallel("styles"));//*.*
+    gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+});
+
+gulp.task("build", gulp.parallel("copy-html", "copy-assets", "build-js", "styles"));
+
+gulp.task("build-prod-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'production',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest(dist));
+});
+
+gulp.task("default", gulp.parallel("watch", "build"));
